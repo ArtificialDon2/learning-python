423e58e070b6f45f88c74fb8b7b6dc3681c9c92b
ArtificialDon2 <apostolovm@abv.bg>
2025-08-17 17:47:00 +0300

    is_palindrome.py

--- 02_is_palindrome/is_palindrome.py
+++ 02_is_palindrome/is_palindrome.py
@@ -7,3 +7,14 @@ def is_palindrome(text):
 input = input('Enter text: ')
 result = is_palindrome(input)
 print(result)
+
+
+// version 2
+import string
+def is_palindrome(text):
+    text = text.translate(str.maketrans("", "", string.punctuation)).lower().replace(" ", "")
+    if text == text[::-1]:
+        return "This is a palindrome."
+    else:
+        return "This is not a palindrome."
+print(is_palindrome(input("Check if palindrome: ")))
